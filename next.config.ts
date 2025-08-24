ec0cf1e235b039126a967fd0638b357eacb3af0b
Coleman Sandefur <coleman.sandefur@gmail.com>
2025-08-24 15:58:21 -0500

    Enable posthog

--- next.config.ts
+++ next.config.ts
@@ -7,4 +7,18 @@ const withSerwist = withSerwistInit({
 
 export default withSerwist({
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 });
